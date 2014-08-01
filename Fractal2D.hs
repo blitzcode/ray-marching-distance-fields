3f128c779839c9b1e919fe6e375cba203bb0dd9b
Tim C. Schroeder <tim@blitzcode.net>
2014-08-01 10:19:25 +0200

    Added animated julia set to Fractal2D module

--- Fractal2D.hs
+++ Fractal2D.hs
@@ -1,5 +1,8 @@
 
+{-# LANGUAGE BangPatterns #-}
+
 module Fractal2D ( mandelbrot
+                 , juliaAnimated
                  ) where
 
 import Control.Loop
@@ -14,7 +17,7 @@ magnitudeSq c = realPart c * realPart c + imagPart c * imagPart c
 -- http://linas.org/art-gallery/escape/escape.html
 -- http://en.wikipedia.org/wiki/Mandelbrot_set#Continuous_.28smooth.29_coloring
 fractionalIterCnt :: Int -> Complex Float -> Float
-fractionalIterCnt iter escZ = fromIntegral iter - (log (log $ magnitudeSq escZ)) / log 2
+fractionalIterCnt iter escZ = max 0 $ fromIntegral iter - (log (log $ magnitudeSq escZ)) / log 2
 
 -- http://en.wikipedia.org/wiki/Mandelbrot_set#Computer_drawings
 mandelbrot :: Int -> Int -> VSM.IOVector Word32 -> Bool -> IO ()
@@ -48,12 +51,39 @@ mandelbrot w h fb smooth =
 
 -- http://en.wikipedia.org/wiki/Julia_set
 -- http://www.relativitybook.com/CoolStuff/julia_set.html
-{-
-    let scaledTick = snd . properFraction $ tick / 17
-        scaledTick2 = snd . properFraction $ tick / 61
-        scaledTick3 = snd . properFraction $ tick / 71
-        twoPi  = scaledTick * 2 * pi
-        juliaR = sin twoPi * max 0.7 scaledTick2
-        juliaI = cos twoPi * max 0.7 scaledTick3
--}
+juliaAnimated :: Int -> Int -> VSM.IOVector Word32 -> Bool -> Double -> IO ()
+juliaAnimated w h fb smooth tick =
+  let !fTick       = realToFrac tick :: Float
+      !scaledTick  = snd (properFraction $ fTick / 17 :: (Int, Float))
+      !scaledTick2 = snd (properFraction $ fTick / 61 :: (Int, Float))
+      !scaledTick3 = snd (properFraction $ fTick / 71 :: (Int, Float))
+      !twoPi       = scaledTick * 2 * pi
+      !juliaR      = sin twoPi * max 0.7 scaledTick2
+      !juliaI      = cos twoPi * max 0.7 scaledTick3
+      !fw          = fromIntegral w  :: Float
+      !fh          = fromIntegral h  :: Float
+      !ratio       = fw / fh
+      !xshift      = 1.45 * ratio
+      !maxIter     = 40
+   in forLoop 0 (< h) (+ 1) $ \py -> forLoop 0 (< w) (+ 1) $ \px ->
+        let idx          = px + py * w
+            fpx          = fromIntegral px :: Float
+            fpy          = fromIntegral py :: Float
+            y            = (fpy / fh) * 2.9 - 1.45 -- Y axis is [-1.45, +1.45]
+            x            = (fpx / fw) * 2.9 * ratio - xshift -- Keep aspect and center
+            c            = x :+ y
+            (iCnt, escZ) = go (0 :: Int) c
+            go iter z | (iter == maxIter) || -- Iteration limit?
+                        magnitudeSq z > 4 * 4 = (iter, z) -- Hit escape radius?
+                      | otherwise = let newZ = z * z + (juliaR :+ juliaI)
+                                     in if   newZ == z -- Simple 1-cycle detection
+                                        then (maxIter, z)
+                                        else go (iter + 1) newZ
+            icCont | iCnt == maxIter = fromIntegral maxIter -- Interior in case of limit
+                   | otherwise       = fractionalIterCnt iCnt escZ
+            toGreen v = v `unsafeShiftL` 8
+         in VSM.unsafeWrite fb idx $
+              if   smooth
+              then toGreen $ truncate (icCont / fromIntegral maxIter * 255 :: Float)
+              else toGreen $ truncate ((fromIntegral iCnt / fromIntegral maxIter) * 255 :: Float)
 
