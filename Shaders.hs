7a6ae2ecfbbc9f6db6252f2db43981e6b579d298
Tim C. Schroeder <tim@blitzcode.net>
2014-08-02 20:21:14 +0200

    Use Quasi Quoter to make editing in-program GLSL shader source less awkward

--- Shaders.hs
+++ Shaders.hs
@@ -1,5 +1,5 @@
 
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
 
 module Shaders ( vsSrcBasic
                , fsSrcBasic
@@ -24,6 +24,7 @@ import Foreign.Storable
 import Foreign.Marshal.Array
 
 import GLHelpers
+import QQPlainText
 
 -- GLSL shaders and support functions
 
@@ -106,42 +107,42 @@ setOrtho2DProjMatrix prog uniform w h = do
 
 -- Shader source for basic vertex and fragment shaders
 vsSrcBasic, fsSrcBasic, fsColOnlySrcBasic :: B.ByteString
-vsSrcBasic = TE.encodeUtf8 $ T.unlines
-    [ "#version 330 core"
-    , "uniform mat4 in_mvp;"
- -- , "layout(location = 0) in vec3 in_pos;"
- -- , "layout(location = 1) in vec4 in_col;"
- -- , "layout(location = 2) in vec2 in_uv;"
-    , "in vec3 in_pos;"
-    , "in vec4 in_col;"
-    , "in vec2 in_uv;"
-    , "out vec4 fs_col;"
-    , "out vec2 fs_uv;"
-    , "void main()"
-    , "{"
-    , "    gl_Position = in_mvp * vec4(in_pos, 1.0);"
-    , "    fs_col      = in_col;"
-    , "    fs_uv       = in_uv;"
-    , "}"
-    ]
-fsSrcBasic = TE.encodeUtf8 $ T.unlines
-    [ "#version 330 core"
-    , "in vec4 fs_col;"
-    , "in vec2 fs_uv;"
-    , "uniform sampler2D tex;"
-    , "out vec4 frag_color;"
-    , "void main()"
-    , "{"
-    , "   frag_color = fs_col * texture(tex, fs_uv);"
-    , "}"
-    ]
-fsColOnlySrcBasic = TE.encodeUtf8 $ T.unlines
-    [ "#version 330 core"
-    , "in vec4 fs_col;"
-    , "out vec4 frag_color;"
-    , "void main()"
-    , "{"
-    , "   frag_color = fs_col;"
-    , "}"
-    ]
+
+vsSrcBasic = TE.encodeUtf8 . T.pack $
+    [plaintext| #version 330 core
+                uniform mat4 in_mvp;
+                in vec3 in_pos;
+                in vec4 in_col;
+                in vec2 in_uv;
+                out vec4 fs_col;
+                out vec2 fs_uv;
+                void main()
+                {
+                    gl_Position = in_mvp * vec4(in_pos, 1.0);
+                    fs_col      = in_col;
+                    fs_uv       = in_uv;
+                }
+    |]
+
+fsSrcBasic = TE.encodeUtf8 . T.pack $
+    [plaintext| #version 330 core
+                in vec4 fs_col;
+                in vec2 fs_uv;
+                uniform sampler2D tex;
+                out vec4 frag_color;
+                void main()
+                {
+                   frag_color = fs_col * texture(tex, fs_uv);
+                }
+    |]
+
+fsColOnlySrcBasic =  TE.encodeUtf8 . T.pack $
+    [plaintext| #version 330 core
+                in vec4 fs_col;
+                out vec4 frag_color;
+                void main()
+                {
+                   frag_color = fs_col;
+                }
+    |]
 
