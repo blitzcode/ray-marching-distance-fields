3d4d2a3b1b9a36c15f92b0b5d7cc425fa02d05de
Tim C. Schroeder <tim@blitzcode.net>
2014-08-08 15:41:15 +0200

    Added support for rendering into the frame buffer texture

--- FrameBuffer.hs
+++ FrameBuffer.hs
@@ -1,5 +1,5 @@
 
-{-# LANGUAGE RecordWildCards, FlexibleContexts #-}
+{-# LANGUAGE RecordWildCards, FlexibleContexts, LambdaCase #-}
 
 module FrameBuffer ( withFrameBuffer
                    , fillFrameBuffer
@@ -7,6 +7,7 @@ module FrameBuffer ( withFrameBuffer
                    , saveFrameBufferToPNG
                    , resizeFrameBuffer
                    , getFrameBufferDim
+                   , drawIntoFrameBuffer
                    , FrameBuffer
                    ) where
 
@@ -15,10 +16,12 @@ import Control.Monad.Trans
 import Control.Monad.Trans.Control
 import Control.Applicative
 import qualified Graphics.Rendering.OpenGL as GL
+import qualified Graphics.Rendering.OpenGL.Raw as GLR
 import Data.Word
 import Data.IORef
 import qualified Data.Vector.Storable.Mutable as VSM
 import qualified Data.Vector.Storable as VS
+import Text.Printf
 import Foreign.Storable
 import Foreign.Ptr
 import Foreign.ForeignPtr
@@ -34,17 +37,25 @@ import Trace
 data FrameBuffer = FrameBuffer { fbTex :: !GL.TextureObject
                                , fbPBO :: !GL.BufferObject
                                , fbDim :: IORef (Int, Int)
+                               , fbFBO :: !GL.FramebufferObject
                                }
 
 withFrameBuffer :: Int -> Int -> (FrameBuffer -> IO a) -> IO a
 withFrameBuffer w h f = do
     traceOnGLError $ Just "withFrameBuffer begin"
     r <- bracket GL.genObjectName GL.deleteObjectName $ \fbTex ->
-         bracket GL.genObjectName GL.deleteObjectName $ \fbPBO -> do
+         bracket GL.genObjectName GL.deleteObjectName $ \fbPBO ->
+         bracket GL.genObjectName GL.deleteObjectName $ \fbFBO -> do
+             -- Setup texture
              GL.textureBinding GL.Texture2D GL.$= Just fbTex
              setTextureFiltering TFMagOnly -- Otherwise we'd need to use automatic MIP-map gen.
              setTextureClampST -- No wrap-around artifacts at the FB borders
-             GL.bindBuffer GL.PixelUnpackBuffer GL.$= Nothing
+             -- Setup FBO
+             GL.bindFramebuffer GL.Framebuffer GL.$= fbFBO
+             GL.framebufferTexture2D GL.Framebuffer (GL.ColorAttachment 0) GL.Texture2D fbTex 0
+             GL.drawBuffer GL.$= GL.FBOColorAttachment 0
+             GL.bindFramebuffer GL.Framebuffer GL.$= GL.defaultFramebufferObject
+             -- Inner
              traceOnGLError $ Just "withFrameBuffer begin inner"
              fbDim <- newIORef (w, h)
              f FrameBuffer { .. }
@@ -57,6 +68,7 @@ resizeFrameBuffer fb w h = writeIORef (fbDim fb) (w, h)
 getFrameBufferDim :: FrameBuffer -> IO (Int, Int)
 getFrameBufferDim fb = readIORef $ fbDim fb
 
+-- Specify the frame buffer contents by filling a mutable vector
 fillFrameBuffer :: (MonadBaseControl IO m, MonadIO m)
                 => FrameBuffer
                 -> (Int -> Int -> VSM.MVector s Word32 -> m a) -- Run inner inside the base monad
@@ -86,21 +98,49 @@ fillFrameBuffer fb@(FrameBuffer { .. }) f = do
     liftIO $ do
       -- Update frame buffer texture from the PBO data
       GL.textureBinding GL.Texture2D GL.$= Just fbTex
-      -- TODO: Could use immutable textures through glTexStorage + glTexSubImage
-      liftIO $ GL.texImage2D GL.Texture2D
-                             GL.NoProxy
-                             0
-                             GL.RGBA8
-                             (GL.TextureSize2D (fromIntegral w) (fromIntegral h))
-                             0
-                             (GL.PixelData GL.RGBA GL.UnsignedByte nullPtr)
+      texImage2DNullPtr w h
       -- Done
       GL.bindBuffer GL.PixelUnpackBuffer GL.$= Nothing
       GL.textureBinding GL.Texture2D GL.$= Nothing
     return r
 
+-- TODO: Could use immutable textures through glTexStorage + glTexSubImage
+texImage2DNullPtr :: Int -> Int -> IO ()
+texImage2DNullPtr w h =
+    GL.texImage2D GL.Texture2D
+                  GL.NoProxy
+                  0
+                  GL.RGBA8
+                  (GL.TextureSize2D (fromIntegral w) (fromIntegral h))
+                  0
+                  (GL.PixelData GL.RGBA GL.UnsignedByte nullPtr)
+
+-- Specify the frame buffer contents by rendering into it
+-- http://www.opengl-tutorial.org/intermediate-tutorials/tutorial-14-render-to-texture/
+drawIntoFrameBuffer :: (MonadBaseControl IO m, MonadIO m)
+                    => FrameBuffer
+                    -> (Int -> Int -> m a)
+                    -> m (Maybe a)
+drawIntoFrameBuffer FrameBuffer { .. } f = do
+    control $ \run -> finally
+        ( do GL.bindFramebuffer GL.Framebuffer GL.$= fbFBO
+             GL.textureBinding  GL.Texture2D   GL.$= Just fbTex
+             (w, h) <- readIORef fbDim
+             texImage2DNullPtr w h
+             -- GL.framebufferStatus is unfortunately broken in OpenGL 2.9.2.0
+             -- (see https://github.com/haskell-opengl/OpenGL/issues/51), so
+             -- we're using the raw APIs as a backup
+             GLR.glCheckFramebufferStatus GLR.gl_FRAMEBUFFER >>= \case
+                 r | r == GLR.gl_FRAMEBUFFER_COMPLETE -> run $ Just <$> f w h
+                 r -> do traceS TLError $ printf
+                             "drawIntoFrameBuffer, glCheckFramebufferStatus: 0x%x"
+                             (fromIntegral r :: Int)
+                         run $ return Nothing
+        )
+        ( GL.bindFramebuffer GL.Framebuffer GL.$= GL.defaultFramebufferObject )
+
 drawFrameBuffer :: FrameBuffer -> QuadRenderBuffer -> Float -> Float -> Float -> Float -> IO ()
-drawFrameBuffer FrameBuffer { .. } qb x1 y1 x2 y2  =
+drawFrameBuffer FrameBuffer { .. } qb x1 y1 x2 y2 =
     -- Draw quad with frame buffer texture
     drawQuad qb
              x1 y1 x2 y2
